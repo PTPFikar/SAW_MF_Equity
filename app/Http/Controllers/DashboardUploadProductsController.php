6e7bcc98e14411fbf80d0fd13adc9dd35b83f989
mohramdhani <velrest48@gmail.com>
2023-10-19 09:14:49 +0700

    update

--- app/Http/Controllers/DashboardUploadProductsController.php
+++ app/Http/Controllers/DashboardUploadProductsController.php
@@ -24,13 +24,33 @@ class DashboardUploadProductsController extends Controller
         $request->validate([
             'file' => 'required|file|mimes:csv,txt',
         ]);
-    
+
         $file = $request->file('file');
 
-        $csv = Reader::createFromPath($file->getPathname(), 'r'); 
+        // Cek tipe file
+        if (!$file->isValid()) {
+            return back()->with('failed', 'File yang diunggah bukan berkas CSV atau teks.');
+        }
+
+        $csv = Reader::createFromPath($file->getPathname(), 'r');
         $csv->setHeaderOffset(0);
-    
+
+        $errors = []; // Inisialisasi sebagai array kosong
+
         foreach ($csv as $row) {
+            if (
+                !isset($row['ISIN']) ||
+                !isset($row['Product Name']) ||
+                !isset($row['Expect Return 1 Year']) ||
+                !isset($row['Standar Deviasi']) ||
+                !isset($row['Asset Under Management (AUM)']) ||
+                !isset($row['Deviden']) ||
+                !isset($row['Date'])
+            ) {
+                $errors[] = 'Data tidak sesuai: Struktur data dalam CSV tidak sesuai dengan yang diharapkan.';
+                break;
+            }
+
             $isin = $row['ISIN'];
             $productName = $row['Product Name'];
             $expectReturn = floatval($row['Expect Return 1 Year']);
@@ -39,38 +59,50 @@ class DashboardUploadProductsController extends Controller
             $dividend = $row['Deviden'];
             $date = $row['Date'];
 
-            //Check products
-            $product = Products::Where('date', $date)->Where('isin', $isin)->first();
+            // Check if required fields are empty
+            if (empty($isin) || empty($productName) || empty($date)) {
+                $errors[] = 'Missing required field for a product.';
+                break;
+            }
+
+            // Check if standarDeviasi is not zero
+            if ($standarDeviasi == 0) {
+                $errors[] = 'Standar Deviasi should not be zero.';
+                break;
+            }
+
+            // Check if 'dividend' value is valid
+            if ($dividend !== 'YES' && $dividend !== 'NO') {
+                $errors[] = 'Invalid value for Deviden. It should be "YES" or "NO.';
+                break;
+            }
+
+            // Check products
+            $product = Products::where('date', $date)->where('isin', $isin)->first();
+
             if (!$product) {
                 $product = new Products();
                 $product->ISIN = $isin;
                 $product->productName = $productName;
-                if ($standarDeviasi != 0) {
-                    $product->sharpRatio = $expectReturn / $standarDeviasi;
-                } else {
-                    $product->sharpRatio = 0;
-                }
+                $product->sharpRatio = ($standarDeviasi != 0) ? $expectReturn / $standarDeviasi : 0;
                 $product->AUM = $aum;
                 $product->deviden = ($dividend === 'YES') ? 2 : 1;
-                if (!empty($date)) {
-                    $product->date = $date;
-                }
+                $product->date = $date;
                 $product->save();
             } else {
-                if ($standarDeviasi != 0) {
-                    $product->sharpRatio = $expectReturn / $standarDeviasi;
-                } else {
-                    $product->sharpRatio = 0;
-                }
+                $product->sharpRatio = ($standarDeviasi != 0) ? $expectReturn / $standarDeviasi : 0;
                 $product->AUM = $aum;
                 $product->deviden = ($dividend === 'YES') ? 2 : 1;
-                if (!empty($date)) {
-                    $product->date = $date;
-                }
+                $product->date = $date;
                 $product->save();
             }
+        }
 
+        if (count($errors) > 0) {
+            $errorMessage = implode(' ', $errors);
+            return back()->with('failed', $errorMessage);
         }
         return redirect()->back()->with('success', 'CSV data imported successfully.');
     }
+
 }
